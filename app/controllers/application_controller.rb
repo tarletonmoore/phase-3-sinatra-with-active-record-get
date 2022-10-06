2dac4b7d32318c51d74f5c068cd70539c52c60f0
John Tarleton Moore <tarletonmoore@gmail.com>
2022-10-06 12:54:34 -0400

    commit tree 98166408cd8c2daf701a35f3275692553043f781

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,43 @@
-class ApplicationController < Sinatra::Base
+# class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+#   get '/games' do
+#     # get all the games from the database
+#     games = Game.all
+#     # return a JSON response with an array of all the game data
+#     games.to_json
+#   end
+
+#   # get '/games' do
+#   #   games = Game.all.order(:title)
+#   #   games.to_json
+#   # end
+
+#   # get '/games' do
+#   #   games = Game.all.order(:title).limit(10)
+#   #   games.to_json
+#   # end
+
+# end
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # game.to_json(include: { reviews: { include: :user } })
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
